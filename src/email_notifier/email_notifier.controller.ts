1039b07a4fcd1a921b4c8d1a89e2a7d5c56d9023
Azeez Adebayo <debaycisse@gmail.com>
2025-11-11 14:15:54 +0100

    refactor: remove orphaned files
    Removed orphaned files after project restructuring

--- src/email_notifier/email_notifier.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('email-notifier')
-export class EmailNotifierController {}
--- src/email_notifier/email_notifier.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmailNotifierService } from './email_notifier.service';
-import { EmailNotifierController } from './email_notifier.controller';
-
-@Module({
-  providers: [EmailNotifierService],
-  controllers: [EmailNotifierController]
-})
-export class EmailNotifierModule {}
