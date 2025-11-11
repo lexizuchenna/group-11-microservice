39c79a675f70095cd19cfa694cd547e9aa0f5872
Azeez Adebayo <debaycisse@gmail.com>
2025-11-11 21:02:49 +0100

    feat(app-module): integrate email-consumer module

--- email_service/src/app.module.ts
+++ email_service/src/app.module.ts
@@ -5,11 +5,13 @@ import {
   email_notifier_module 
 } from './email_notifier/email_notifier.module';
 import { ConfigModule } from '@nestjs/config';  // to load .env content
+import { email_consumer_module } from './email_consumer/email_consumer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    email_notifier_module
+    email_notifier_module,
+    email_consumer_module
   ],
   controllers: [AppController],
   providers: [AppService],
