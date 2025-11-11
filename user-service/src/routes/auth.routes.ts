6bf6b1f2727c175272babacfca8ebcfd0181b3d4
Alexander <contact@mail.lexiz.is-a.dev>
2025-11-11 11:22:15 +0100

    fix: fix login route endpoint

--- user-service/src/routes/auth.routes.ts
+++ user-service/src/routes/auth.routes.ts
@@ -3,5 +3,5 @@ import { create_user, login_user } from "../controllers/auth.controller";
 
 export const auth_routes = async (server: FastifyInstance) => {
   server.post("/api/v1/auth/login", login_user);
-  server.post("api/v1/auth/register", create_user);
+  server.post("/api/v1/auth/register", create_user);
 };
