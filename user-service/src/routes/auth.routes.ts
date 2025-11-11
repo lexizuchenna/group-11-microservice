3d16360657d80bf0973c3803c4ac4aa9d8e777e0
Alexander <contact@mail.lexiz.is-a.dev>
2025-11-11 10:05:11 +0100

    refactor: move user register handler to auth.controller

--- user-service/src/routes/auth.routes.ts
+++ user-service/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
-import { login_user } from "../controllers/auth.controller";
+import { create_user, login_user } from "../controllers/auth.controller";
 
 export const auth_routes = async (server: FastifyInstance) => {
   server.post("/api/v1/auth/login", login_user);
+  server.post("api/v1/auth/register", create_user);
 };
--- user-service/src/routes/user.routes.ts
+++ user-service/src/routes/user.routes.ts
@@ -1,6 +1,3 @@
 import { FastifyInstance } from "fastify";
-import { create_user } from "../controllers/user.controller";
 
-export const user_routes = async (server: FastifyInstance) => {
-  server.post("/api/v1/users", create_user);
-};
+export const user_routes = async (server: FastifyInstance) => {};
