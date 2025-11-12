1c0df6f4112f7866760804eda16dfdf66e82dde1
Azeez Adebayo <debaycisse@gmail.com>
2025-11-12 13:00:51 +0100

    feat(push-notifier): add /status endpoint to check push service health

--- push_service/src/push_notifier/push_notifier.controller.ts
+++ push_service/src/push_notifier/push_notifier.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { push_notifier_service } from './push_notifier.service';
 
-@Controller('push-notifier')
-export class push_notifier_controller {}
+@Controller('pushes')
+export class push_notifier_controller {
+  constructor(
+    private readonly push_service: push_notifier_service
+  ) {}
+
+  @Get('/status')
+  async get_health() {
+    return await this.push_service.get_health_status()
+  }
+}
