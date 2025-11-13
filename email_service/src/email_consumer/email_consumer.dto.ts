15c2a99ffe9d631dc054dc07c21bda9c6698eafd
Azeez Adebayo <debaycisse@gmail.com>
2025-11-13 06:48:24 +0100

    feat(email-consumer): add DTO for expected message queue for email

---
+++ email_service/src/email_consumer/email_consumer.dto.ts
@@ -0,0 +1,51 @@
+import {
+  IsString,
+  IsOptional,
+  ValidateNested,
+  IsEmail,
+  IsUrl
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+
+export class email_message_queue {
+  @IsString()
+  type: "email";
+
+  @IsEmail()
+  to: string;
+
+  @IsString()
+  subject: string;
+
+  @IsString()
+  body: string;
+
+  @IsOptional()
+  @IsString()
+  template_id?: string;
+
+  @ValidateNested()
+  @Type(() => template_dynamic_placeholder)
+  data: template_dynamic_placeholder;
+
+  @ValidateNested()
+  @Type(() => event_and_timestamp)
+  metadata: event_and_timestamp;
+}
+
+export class template_dynamic_placeholder {
+  @IsString()
+  username: string;
+
+  @IsUrl()
+  activation_link: string;
+}
+
+export class event_and_timestamp {
+  @IsString()
+  event: string;
+
+  @IsString()
+  timestamp: string;
+}
